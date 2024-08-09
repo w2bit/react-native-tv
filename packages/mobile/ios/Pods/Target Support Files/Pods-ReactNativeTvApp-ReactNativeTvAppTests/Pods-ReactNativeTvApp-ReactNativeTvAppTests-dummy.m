8af8f3b1d1c8e65d047a249b1e2420032847bf06
Davidson Simões <davidsonsimoes@Davidsons-MBP-2.local>
2024-08-09 12:31:14 -0300

    initial config

---
+++ packages/mobile/ios/Pods/Target Support Files/Pods-ReactNativeTvApp-ReactNativeTvAppTests/Pods-ReactNativeTvApp-ReactNativeTvAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeTvApp_ReactNativeTvAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeTvApp_ReactNativeTvAppTests
+@end
