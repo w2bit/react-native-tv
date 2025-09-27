79666680627ffe42d2225b405b2de2e0af4a51b7
Davidson Simões <davidson.simoes@gmail.com>
2025-09-27 16:50:27 -0300

    removed deprecated

--- packages/mobile/ios/Pods/Target Support Files/Pods-ReactNativeTvApp-ReactNativeTvAppTests/Pods-ReactNativeTvApp-ReactNativeTvAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeTvApp_ReactNativeTvAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeTvApp_ReactNativeTvAppTests
-@end
